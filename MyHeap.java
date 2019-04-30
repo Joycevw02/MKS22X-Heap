f9daea269db835b596a9959efd19fb63a9b9a787
Joyce Wang <jwang01@stuy.edu>
2019-04-29 21:44:04 -0400

    Wrote push down, doesn't work if the last value is the greatest on one run though

--- MyHeap.java
+++ MyHeap.java
@@ -4,7 +4,27 @@ public class MyHeap{
   //- precondition: index is between 0 and size-1 inclusive
   //- precondition: size is between 0 and data.length-1 inclusive.
   private static void pushDown(int[]data,int size,int index){
-    
+    //Stores the index of the left and right branches
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+    //If the left is in bound and greater than data or if right is in bound and greater than data...
+    if ((left < size && data[index] < data[left]) || (right < size && data[index] < data[right])){
+      //Swap with the larger child
+      if (data[right] > data[left]){
+        int temp = data[right];
+        data[right] = data[index];
+        data[index] = temp;
+        pushDown(data, size, right);
+        pushDown(data, size, index + 1);
+      }
+      else{
+        int temp = data[left];
+        data[left] = data[index];
+        data[index] = temp;
+        pushDown(data, size, left);
+        pushDown(data, size, index + 1);
+      }
+    }
   }
 
   //- push the element at index i up into the correct position. This will swap it with the parent node until the parent node is larger or the root is reached. [ should be O(logn) ]
@@ -26,6 +46,10 @@ public class MyHeap{
   }
 
   public static void main(String[] args){
-
+    int[] test = {12,16,13,13,7,14,40};
+    pushDown(test, test.length - 1, 0);
+    for (int i : test){
+      System.out.println(i + "");
+    }
   }
 }
