22dbdd0b805c8bf554b5b6e5abd64862cffb6900
Joyce Wang <jwang01@stuy.edu>
2019-04-30 20:03:03 -0400

    Wrote push up

--- MyHeap.java
+++ MyHeap.java
@@ -4,7 +4,7 @@ public class MyHeap{
   //- precondition: index is between 0 and size-1 inclusive
   //- precondition: size is between 0 and data.length-1 inclusive.
   private static void pushDown(int[]data,int size,int index){
-    //Stores the index of the left and right branches
+    //Stores the index of the left and right nodes
     int left = 2 * index + 1;
     int right = 2 * index + 2;
     //If the left is in bound and greater than data or if right is in bound and greater than data...
@@ -14,6 +14,7 @@ public class MyHeap{
         int temp = data[right];
         data[right] = data[index];
         data[index] = temp;
+        //Call pushDown on the right branch and the next index
         pushDown(data, size, right);
         pushDown(data, size, index + 1);
       }
@@ -21,6 +22,7 @@ public class MyHeap{
         int temp = data[left];
         data[left] = data[index];
         data[index] = temp;
+        //Call pushDown on the left branch and the next index
         pushDown(data, size, left);
         pushDown(data, size, index + 1);
       }
@@ -30,7 +32,16 @@ public class MyHeap{
   //- push the element at index i up into the correct position. This will swap it with the parent node until the parent node is larger or the root is reached. [ should be O(logn) ]
   //- precondition: index is between 0 and data.length-1 inclusive.
   private static void pushUp(int[]data,int index){
-
+    //Store the index of the parent node
+    int parent = (index - 1) / 2;
+    //If the current index is not zero and its value is greater than the
+    //parent's value, swap the values and pushUp from the new index
+    if (index > 0 && data[index] > data[parent]){
+      int temp = data[parent];
+      data[parent] = data[index];
+      data[index] = temp;
+      pushUp(data, parent);
+    }
   }
 
   //- convert the array into a valid heap. [ should be O(n) ]
@@ -47,7 +58,12 @@ public class MyHeap{
 
   public static void main(String[] args){
     int[] test = {12,16,13,13,7,14,40};
-    pushDown(test, test.length - 1, 0);
+//    for (int i = test.length - 1; i >= 0; i --){
+//      pushDown(test, test.length, i);
+//    }
+    for (int i = 0; i < test.length; i ++){
+      pushUp(test, i);
+    }
     for (int i : test){
       System.out.println(i + "");
     }
